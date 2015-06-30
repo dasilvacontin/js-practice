1342de33467f9e5fb9aa029a49039776cdd81845
pacific <cod.al.pro@gmail.com>
2015-06-30 10:43:32 +0200

    Prototipado de description

--- Rectangle.js
+++ Rectangle.js
@@ -2,13 +2,14 @@ function Rectangle (w, h) {
   this.width = w
   this.height = h
   this.type = 'Rectangle'
-  this.description = function () {
-    return 'Rectangle of width ' + this.width + ', height ' + this.height + ' and area ' + this.area() + '.'
-  }
 }
 
 Rectangle.prototype.area = function () {
   return this.width * this.height
+}
+
+Rectangle.prototype.description = function () {
+  return 'Rectangle of width ' + this.width + ', height ' + this.height + ' and area ' + this.area() + '.'
 }
 
 module.exports = Rectangle
--- Square.js
+++ Square.js
@@ -3,10 +3,12 @@ var Rectangle = require('./Rectangle')
 function Square (side) {
   Rectangle.call(this, side, side)
   this.type = 'Square'
-  this.description = function () {
-    return 'Square of side ' + this.width + ' and area ' + this.area() + '.'
-  }
 }
 
 Square.prototype = new Rectangle()
+
+Square.prototype.description = function () {
+  return 'Square of side ' + this.width + ' and area ' + this.area() + '.'
+}
+
 module.exports = Square
