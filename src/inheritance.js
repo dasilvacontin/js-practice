0a02a1abeb5935421099c97ac47dfd36fabae298
vylion <volfaria@gmail.com>
2015-06-30 10:40:06 +0200

    reordered code to avoid intercalation#

--- src/inheritance.js
+++ src/inheritance.js
@@ -3,18 +3,21 @@ function Rectangle (w, h) {
   this.width = w
   this.height = h
 }
-function Square (n) {
-  Rectangle.call(this, n, n)
-}
 
 Rectangle.prototype.area = function () {
   return this.width * this.height
 }
-Square.prototype = new Rectangle()
 
 Rectangle.prototype.description = function () {
   console.log('Rectangle of width ' + this.width + ', height ' + this.height + ' and area ' + this.area() + '.')
 }
+
+function Square (n) {
+  Rectangle.call(this, n, n)
+}
+
+Square.prototype = new Rectangle()
+
 Square.prototype.description = function () {
   console.log('Square of side ' + this.width + ' and area ' + this.area() + '.')
 }
