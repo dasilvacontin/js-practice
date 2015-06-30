6a9547e27cc56e9443c049c5bd449baa9de6fdba
Drixide77 <adria.abella.93@gmail.com>
2015-06-30 10:21:04 +0200

    Ya funciona :)

---
+++ src/inheritance.js
@@ -0,0 +1,29 @@
+function Rectangle (width, height) {
+  this.width = width
+  this.height = height
+}
+
+Rectangle.prototype.area = function () {
+  return this.width * this.height
+}
+
+Rectangle.prototype.description = function () {
+  return 'Rectangle of width ' + this.width +
+  ', height ' + this.height + ' and area ' + this.area() +
+  '.'
+}
+
+function Square (side) {
+  this.width = this.height = side
+}
+
+Square.prototype = new Rectangle()
+
+Square.prototype.description = function () {
+  return 'Square of side ' + this.width +
+  ' and area ' + this.area() +
+  '.'
+}
+
+module.exports.Rectangle = Rectangle
+module.exports.Square = Square
