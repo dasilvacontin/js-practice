4494fe408855b0ad2b51796f087f493302668da6
pacific <cod.al.pro@gmail.com>
2015-06-30 10:28:29 +0200

    Añadida funcion description y borrado de archivos swp

--- Rectangle.js
+++ Rectangle.js
@@ -1,6 +1,10 @@
 function Rectangle (w, h) {
   this.width = w
   this.height = h
+  this.type = 'Rectangle'
+  this.description = function () {
+    return 'Rectangle of width ' + this.width + ', height ' + this.height + ' and area ' + this.area() + '.'
+  }
 }
 
 Rectangle.prototype.area = function () {
--- Square.js
+++ Square.js
@@ -2,6 +2,10 @@ var Rectangle = require('./Rectangle')
 
 function Square (side) {
   Rectangle.call(this, side, side)
+  this.type = 'Square'
+  this.description = function () {
+    return 'Square of side ' + this.width + ' and area ' + this.area() + '.'
+  }
 }
 
 Square.prototype = new Rectangle()
